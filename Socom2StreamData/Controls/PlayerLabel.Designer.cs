f74f4836dbec636973ae3c451f8d56f8ca35e5f5
1UP <zero1up@gmail.com>
2019-12-19 21:56:22 -0500

    Added options to allow any of the windows to be a top most window. This would allow the window to be visible over top of pcsx2.
    Added Discord rich presence support that will display the following:
    
    Room name
    Current map
    Seals and Terrorist rounds won
    How long you've been in the match
    
    All maps are current supported with a map image. This also goes for Harry62's Day and Night maps.

---
+++ Socom2StreamData/Controls/PlayerLabel.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace Socom2StreamData.Controls
+{
+    partial class PlayerLabel
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pnl_LivingStatus = new System.Windows.Forms.Panel();
+            this.lbl_Name = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.lbl_Name);
+            this.panel1.Controls.Add(this.pnl_LivingStatus);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(61, 21);
+            this.panel1.TabIndex = 0;
+            // 
+            // pnl_LivingStatus
+            // 
+            this.pnl_LivingStatus.BackColor = System.Drawing.Color.Yellow;
+            this.pnl_LivingStatus.Location = new System.Drawing.Point(4, 5);
+            this.pnl_LivingStatus.Name = "pnl_LivingStatus";
+            this.pnl_LivingStatus.Size = new System.Drawing.Size(10, 10);
+            this.pnl_LivingStatus.TabIndex = 0;
+            // 
+            // lbl_Name
+            // 
+            this.lbl_Name.AutoSize = true;
+            this.lbl_Name.ForeColor = System.Drawing.Color.White;
+            this.lbl_Name.Location = new System.Drawing.Point(18, 3);
+            this.lbl_Name.Name = "lbl_Name";
+            this.lbl_Name.Size = new System.Drawing.Size(35, 13);
+            this.lbl_Name.TabIndex = 1;
+            this.lbl_Name.Text = "Name";
+            // 
+            // PlayerLablecs
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Transparent;
+            this.Controls.Add(this.panel1);
+            this.Name = "PlayerLablecs";
+            this.Size = new System.Drawing.Size(61, 21);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lbl_Name;
+        private System.Windows.Forms.Panel pnl_LivingStatus;
+    }
+}
---
+++ Socom2StreamData/Controls/PlayerLabel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Socom2StreamData.Controls
+{
+    public partial class PlayerLabel : UserControl
+    {
+        public Color livingStatus
+        {
+
+            set
+            {
+                pnl_LivingStatus.BackColor = value;
+               
+            }
+        }
+        public string playerName
+        {
+            
+            set { lbl_Name.Text = value; }
+        }
+
+        public Color playerNameColor
+        {
+            set { lbl_Name.ForeColor = value; }
+        }
+        public PlayerLabel()
+        {
+            InitializeComponent();
+        }
+    }
+}
